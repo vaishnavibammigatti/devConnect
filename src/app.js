1fafbf55aff3973149526017bed32b54feda67c6
Vaishnavi N Bammigatti <vaishnavibammigatti@gmail.com>
2026-05-04 20:02:37 +0530

    Schema validations

--- src/app.js
+++ src/app.js
@@ -3,19 +3,61 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Pavitra",
-    lastName: "patil",
-    emailId: "pavvi@g.com",
-    password: "234",
-  });
+app.use(express.json()); //Converts JSON to Javascript object
 
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
   try {
     await user.save();
     res.status(201).send("User Created successfully", user);
   } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+//get user by emailId
+app.get("/getUserByEmailId", async (req, res) => {
+  try {
+    const user = await User.find({ emailId: req.body.emailId });
+    if (user.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.status(200).send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+//get all users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (err) {
     res.status(400).send("Error occured ", err.message);
+  }
+});
+
+app.patch("/patchUser", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndUpdate(userId, req.body, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.status(200).send("Updated successfully " + user);
+  } catch (err) {
+    res.status(400).send("Update failed " + err.message);
+  }
+});
+
+app.delete("/deleteUser", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.body.userId);
+    res.status(204).send("Deleted sucessully" + user);
+  } catch (err) {
+    res.status(400).send("something went wrong");
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,55 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is not valid!");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://img.magnific.com/premium-vector/default-avatar-profile-icon-gray-placeholder-vector-illustration_514344-14757.jpg?semt=ais_hybrid&w=740&q=80",
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
+  {
+    timestamps: true,
   },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+);
 
 module.exports = mongoose.model("User", userSchema);
